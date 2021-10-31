7cf42076b43d1d1bbd353e5304c06352abe7a849
tarun14 <tarun0xe@gmail.com>
2021-11-01 04:00:21 +0530

    deletenodeatpos,insertnodeatpos functions are added;code is structured

--- singlylinkedlist/singlylinkedlist.c
+++ singlylinkedlist/singlylinkedlist.c
@@ -37,50 +37,23 @@ typedef struct node{
 /* 'Start' pointer for SLL */
 node *Start = NULL;
 
-void freelist(void)
-{
-    node *pos, *next_node;
-
-    pos = Start;
-    /* Start should point to 'Null' */
-    Start = NULL;
-    printf("Freeing the list!!!\n");
-    if(pos == NULL){
-        printf("The list was already empty, exiting now...\n");
-    }
-    else{
-        /* Delete all nodes till end */
-        while(pos->next != NULL){
-        /* Store next node's address */
-        next_node = pos->next;
-        /* Free current node */
-        free(pos);
-        /* Move to next node */
-        pos=next_node;
-        }
-        /* Free last node */
-        free(pos);
-        printf("The list is freed, exiting now...\n");
-    }
-    return;
-}
 /* Show nodes of SLL */
 void showlist(void)
 {
-	node *pos;
-	/* Store value of 'Start' in 'temp' variable */
-	pos = Start;
-	if(pos == NULL){
-		printf("The list is empty\n");
-	}
-	else{
-		for(;pos!=NULL;pos = pos->next){
-			/* Print value of node */
-			printf("%d ",pos->val);
-		}
-		printf("\n");
-	}
-	return;
+    node *pos;
+    /* Store value of 'Start' in 'temp' variable */
+    pos = Start;
+    if( pos == NULL ){
+        printf("The list is empty\n");
+    }   
+    else{
+        for(;pos!=NULL;pos = pos->next){
+            /* Print value of node */
+            printf("%d ",pos->val);
+        }
+        printf("\n");
+    }   
+    return;
 }
 
 /* Add node to SLL at beginning */
@@ -102,7 +75,7 @@ void insertnodeatbeg(void)
     temp->val = val;
 
 	/* Insert new node at beg of list */
-	if(pos == NULL){
+	if( pos == NULL ){
 		/* If it was an empty list */
 		temp->next = NULL;
 		/* Now 'Start' must point to newly added node */
@@ -131,17 +104,80 @@ void insertnodeatend(void)
     scanf("%d",&(temp->val));
 
     /* Empty list */
-    if(pos == NULL){
+    if( pos == NULL ){
         temp->next = NULL;
         Start = temp;
     }
     else{
         /* Insert new node at end */
-        while(pos->next != NULL){
+        while( pos->next != NULL ){
             pos = pos->next;
         }
         temp->next = NULL;
         pos->next = temp;
+    }
+    return;
+}
+
+/* Insert node at nth position */
+void insertnodeatpos(void)
+{
+    int insert_pos,i,val;
+    node *curr_node, *prev_node, *temp_node;
+    /* Init current node with first node */
+    curr_node = Start;
+
+    printf("Select the node position at which new node to be inserted in below list:\n");
+    showlist();
+    printf(":");
+    scanf("%d",&insert_pos);
+
+    printf("Enter an integer value of node:");
+    scanf("%d",&val);
+
+    /* A new node is created from heap */
+    temp_node = (node *)malloc(sizeof(node));
+    /* Store received value into node */
+    temp_node->val = val;
+
+    /* List is empty */
+    if( curr_node == NULL ){
+        temp_node->next = NULL;
+        Start = temp_node;
+        printf("Node inserted at pos:0\n");
+    }
+    else{
+        /* If node to be inserted at 0th position */
+        if( insert_pos == 0 ){
+            temp_node->next = curr_node;
+            Start = temp_node;
+            printf("Node inserted at pos:0\n");
+        }
+        else{
+            /* check till we don't reach list end */
+            for( i=0; curr_node->next != NULL; i++,curr_node = curr_node->next ){
+                /* i-th node is to be inserted */
+                if( insert_pos == i ){
+                    temp_node->next = curr_node;
+                    prev_node->next = temp_node;
+                    printf("Node inserted at pos:%d\n",i);
+                    return;
+                }
+                else{
+                    prev_node = curr_node;
+                }
+            }
+            /* The last node to be inserted */
+            if( insert_pos == i ){
+                temp_node->next = curr_node;
+                prev_node->next = temp_node;
+                printf("Node inserted at pos:%d\n",i);
+            }
+            /* The position is higher than list size */
+            else{
+                printf("The position entered is out of list size range!!!\n");
+            }
+        }
     }
     return;
 }
@@ -154,7 +190,7 @@ void deletenodeatbeg(void)
 	/* Store current value of Start */
     pos = Start;
 
-    if(pos == NULL){
+    if( pos == NULL ){
         printf("The list is already empty\n");
     }
     else{
@@ -165,6 +201,7 @@ void deletenodeatbeg(void)
 
     return;
 }
+
 /* Delete node at end */
 void deletenodeatend(void)
 {
@@ -172,17 +209,17 @@ void deletenodeatend(void)
     pos = Start;
 
     /* The list is empty */
-    if(pos == NULL){
+    if( pos == NULL ){
         printf("The list is already empty\n");
     }
     else{
-        while(pos->next != NULL){
+        while( pos->next != NULL ){
             prev_node = pos;
             pos = pos->next;
         }
         /* Only node of single node list is deleted */
         /* List is empty now */
-        if(Start->next == NULL){
+        if( Start->next == NULL ){
             Start=NULL;
         }
         /* Last node deleted, prev node will now be last node */
@@ -195,15 +232,91 @@ void deletenodeatend(void)
     }
     return;
 }
+
+/* Delete node at nth position */
 void deletenodeatpos(void)
 {
-    int pos;
+    int dlt_pos,i;
+    node *curr_node, *prev_node;
+    /* Init current node with first node */
+    curr_node = Start;
 
     printf("Select the node position to be deleted in below list:\n");
     showlist();
     printf(":");
-    scanf
+    scanf("%d",&dlt_pos);
 
+    /* List is empty */
+    if( curr_node == NULL ){
+        printf("List is already empty!!!\n");
+    }
+    else{
+        /* First node to be deleted */
+        if( dlt_pos == 0 ){
+            Start = curr_node->next;
+            free(curr_node);
+            printf("First node deleted\n");
+        }
+        else{
+            /* check till we don't reach list end */
+            for( i=0; curr_node->next != NULL; i++,curr_node = curr_node->next ){               
+                /* i-th node is to be deleted */
+                if( dlt_pos == i ){
+                    prev_node->next = curr_node->next;
+                    free(curr_node);
+                    printf("Node at pos:%d is deleted\n",i);
+                    return;
+                }
+                else{
+                    prev_node = curr_node;
+                }
+            }
+            /* The last node to be deleted */
+            if( dlt_pos == i ){
+                prev_node->next = NULL;
+                free(curr_node);
+                printf("The last node deleted\n");
+            }
+            /* The position is higher than list size */
+            else{
+                printf("The position entered is out of list size range!!!\n");
+            }
+        }
+    }
+    return;
+}
+
+void searchnodeatpos(void)
+{
+    return;
+}
+/* Free all nodes of SLL */
+void freelist(void)
+{   
+    node *pos, *next_node;
+    
+    pos = Start;
+    /* Start should point to 'Null' */
+    Start = NULL;
+    printf("Freeing the list!!!\n");
+    if( pos == NULL ){
+        printf("The list was already empty, exiting now...\n");
+    }
+    else{
+        /* Delete all nodes till end */
+        while(pos->next != NULL){
+        /* Store next node's address */
+        next_node = pos->next;
+        /* Free current node */
+        free(pos);
+        /* Move to next node */
+        pos=next_node;
+        }
+        /* Free last node */
+        free(pos);
+        printf("The list is freed, exiting now...\n");
+    }
+    return;
 }
 
 /* MAIN function */
@@ -214,20 +327,20 @@ int main()
     int val=0,input=0;
     bool exit=false;
 
-
     printf("***** Singly Linked List *******\n");
     
     while(1){
         printf("\nWhich operation do you want to perform??\n");
         printf("1.Insert Node at beginning\n");
         printf("2.Insert Node at end\n");
-        printf("3.Delete Node at beginning\n");
-        printf("4.Delete Node at end\n");
-        printf("5.Delete Node at nth position(pos=1 for 1st element\n");
-        printf("6.Search position of a given node\n");
-        printf("7.Display the linked list\n");
-        printf("8.Reverse the linked list\n");
-        printf("9.Quit\n\n:");
+        printf("3.Insert Node at nth position(pos=0 for 1st element\n");
+        printf("4.Delete Node at beginning\n");
+        printf("5.Delete Node at end\n");
+        printf("6.Delete Node at nth position(pos=0 for 1st element\n");
+        printf("7.Search position of a given node\n");
+        printf("8.Display the linked list\n");
+        printf("9.Reverse the linked list\n");
+        printf("10.Quit\n\n:");
 
         scanf("%d",&input);
         
@@ -244,33 +357,43 @@ int main()
             printf("List after insertion at end\n:");
             showlist();
             break;
-        /* Delete node at beginning */
+        /* Insert node at nth position */
         case 3:
+            insertnodeatpos();
+            printf("List after insertion at entered position\n:");
+            showlist();
+            break;
+        /* Delete node at beginning */
+        case 4:
             deletenodeatbeg();
             printf("List after deletion at beginning\n:");
             showlist();
             break;
         /* Delete node at end */
-        case 4:
+        case 5:
             deletenodeatend();
             printf("List after deletion at end\n:");
             showlist();
             break;
         /* Delete node at nth position */
-        case 5:
+        case 6:
+            deletenodeatpos();
+            printf("list after deletion at entered position\n:");
+            showlist();
             break;
         /* Search position of a given node */
-        case 6:
+        case 7:
+            searchnodeatpos();
             break;
         /* Display linked list */
-        case 5:
+        case 8:
             showlist();
             break;
         /* Reverse linked list */
-        case 6:
+        case 9:
             break;
         /* Quit, free all nodes */
-        case 7:
+        case 10:
             freelist();
             exit = true;
             break;
